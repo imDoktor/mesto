a9288a96c4d939bf2a0962574b1b93285da3198a
CeberSobgusto <denekin.nikita@bk.ru>
2020-07-09 22:12:00 +0300

    refactor: изменить функции. fix: изменить классы по БЭМ

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let editBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let btnClose = document.querySelector('.popup__btn_close');
-let popupName = document.querySelector('.popup__name');
-let popupJob = document.querySelector('.popup__job');
+let popupName = document.querySelector('.popup__input_value-name');
+let popupJob = document.querySelector('.popup__input_value-job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let btnSave = document.querySelector('.popup__btn_save');
@@ -10,29 +10,32 @@ let formPopup = document.querySelector('.popup__form');
 
 
 editBtn.addEventListener('click', function() {
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
 })
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
 
 btnClose.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
+    closePopup ();
 })
 
 popupName.value = profileName.textContent;
 popupJob.value = profileJob.textContent;
 
 function replaceValue (){
-    profileName.textContent = popupName.value
-    profileJob.textContent = popupJob.value
+    profileName.textContent = popupName.value;
+    profileJob.textContent = popupJob.value;
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profileName.textContent = popupName.value
-    profileJob.textContent = popupJob.value
+    profileName.textContent = popupName.value;
+    profileJob.textContent = popupJob.value;
 }
 formPopup.addEventListener('submit', formSubmitHandler);
 
 btnSave.addEventListener('click', function(){
-    replaceValue ()
+    replaceValue ();
     popup.classList.remove('popup_opened');
 });
