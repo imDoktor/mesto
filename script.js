4efe5b7429fda3ec36593dbb6a2f5b9450a7126a
CeberSobgusto <denekin.nikita@bk.ru>
2020-07-15 16:33:53 +0300

    fixed: исправить Js и другие недочеты

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-let editBtn = document.querySelector('.profile__btn_edit');
+let editBtn = document.querySelector('.profile__btn_action-edit');
 let popup = document.querySelector('.popup');
-let btnClose = document.querySelector('.popup__btn_close');
-let popupName = document.querySelector('.popup__name');
-let popupJob = document.querySelector('.popup__job');
+let btnClose = document.querySelector('.popup__btn_action-close');
+let popupName = document.querySelector('.popup__input_value-name');
+let popupJob = document.querySelector('.popup__input_value-job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let btnSave = document.querySelector('.popup__btn_save');
+let btnSave = document.querySelector('.popup__btn_action-save');
 let formPopup = document.querySelector('.popup__form');
 
 function openedOrClose (){
-    if(popup.classList.contains('popup__opened')){
-        popup.classList.remove('popup__opened');
+    if(popup.classList.contains('popup_opened')){
+        popup.classList.remove('popup_opened');
     }
     else{
-        popup.classList.add('popup__opened');
+        popup.classList.add('popup_opened');
         popupName.value = profileName.textContent;
         popupJob.value = profileJob.textContent; 
     }
 };
 
-function copyAndClose (){
-    profileName.textContent = popupName.value;
-    profileJob.textContent = popupJob.value;
-    openedOrClose ();
-};
 
 editBtn.addEventListener('click', openedOrClose);
 btnClose.addEventListener('click', openedOrClose);
-btnSave.addEventListener('click', copyAndClose);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileJob.textContent = popupJob.value;
+    openedOrClose ();
 }
 formPopup.addEventListener('submit', formSubmitHandler);
